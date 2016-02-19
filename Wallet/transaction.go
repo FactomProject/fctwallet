cd7fd1ac9e06d7969a83b0769df0307a6a80687c
ThePiachu <ThePiachu@gmail.com>
2016-02-18 17:00:27 -0800

    Fix #2.

--- Wallet/transaction.go
+++ Wallet/transaction.go
@@ -15,7 +15,6 @@ import (
 	"github.com/FactomProject/factomd/common/constants"
 	"github.com/FactomProject/factomd/common/factoid"
 	"github.com/FactomProject/factomd/common/interfaces"
-	"github.com/FactomProject/factomd/common/primitives"
 	"github.com/FactomProject/fctwallet/Wallet/Utility"
 	"github.com/FactomProject/fctwallet/scwallet"
 )
